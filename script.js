6522ff7e6c729622320e310743116885d867882e
palkanis <palkanis@msu.edu>
2025-03-06 22:38:12 +0530

    Fix container size to exact dimensions
    Put logic in loop so all squares get made in limited space

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 //Etch-a-Sketch
 const container = document.querySelector('.container');
+container.style.width = '600' + 'px';
+container.style.height = container.style.width;
 
-for (let i = 0; i < 256; i++) {
+const numOfSq = prompt('Input grid pattern:');
+
+
+for (let i = 0; i < (numOfSq**2); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.classList.add('hover')
+    square.classList.add('hover');
+
+    areaTtl = (Number(container.style.width.slice(0,3)))**2;
+    areaSq = areaTtl/(numOfSq**2);
+    lenSq = Math.sqrt(areaSq);
+
+    square.style.boxSizing = 'border-box';
+    square.style.width = lenSq.toString() + 'px';
+    square.style.height = lenSq.toString() + 'px';
 
     container.appendChild(square);
 }
+
 
 const hover = document.getElementsByClassName('hover');
 
