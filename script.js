b4e2256cab5f3b0f5dd398989a7515b776f83497
palkanis <palkanis@msu.edu>
2025-03-06 09:44:33 +0530

    Add hover effect
    Use a const container to store all divs
    
    Use a for loop to add event listener to all divs

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
-    const sqaure = document.createElement('div');
-    sqaure.classList.add('square')
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.classList.add('hover')
 
-    container.appendChild(sqaure);
+    container.appendChild(square);
 }
+
+const hover = document.getElementsByClassName('hover');
+
+for (let i = 0; i < hover.length; i++)
+    hover[i].addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'gray';
+})
+
+
