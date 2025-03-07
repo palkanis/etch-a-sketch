858c874d233eacef3f57a92836754e5e06be4c80
palkanis <palkanis@msu.edu>
2025-03-07 21:42:37 +0530

    Add reset button
    Add change grid button

--- script.js
+++ script.js
@@ -3,31 +3,60 @@ const container = document.querySelector('.container');
 container.style.width = '600' + 'px';
 container.style.height = container.style.width;
 
-const numOfSq = prompt('Input grid pattern:');
+const inputBtn = document.querySelector('#input');
+inputBtn.addEventListener('click', () => {
+    numOfSq = prompt('Input');
+    if (numOfSq > 100) {
+        alert('Not a valid choice');
+    }
+    else {
+        updateGrid();
+        createGrid(numOfSq);
+    }
+})
 
 
-for (let i = 0; i < (numOfSq**2); i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.classList.add('hover');
 
-    areaTtl = (Number(container.style.width.slice(0,3)))**2;
-    areaSq = areaTtl/(numOfSq**2);
-    lenSq = Math.sqrt(areaSq);
 
-    square.style.boxSizing = 'border-box';
-    square.style.width = lenSq.toString() + 'px';
-    square.style.height = lenSq.toString() + 'px';
+function updateGrid() {
+    container.innerHTML = '';
+}
 
-    container.appendChild(square);
+function createGrid(size = 16) {
+    for (let i = 0; i < (size**2); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add('hover');
+
+        areaTtl = (Number(container.style.width.slice(0,3)))**2;
+        areaSq = areaTtl/(size**2);
+        lenSq = Math.sqrt(areaSq);
+
+        square.style.boxSizing = 'border-box';
+        square.style.width = lenSq.toString() + 'px';
+        square.style.height = lenSq.toString() + 'px';
+
+        container.appendChild(square);
+    }
+    const hover = document.getElementsByClassName('hover');
+    const reset = document.querySelector('#reset');
+
+
+    for (let i = 0; i < hover.length; i++) {
+        hover[i].addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'gray';
+    });
+    }
+
+    reset.addEventListener('click', () => {
+        for (let i = 0; i < hover.length; i++)
+            hover[i].style.backgroundColor = 'white';
+    })
+
 }
 
 
-const hover = document.getElementsByClassName('hover');
+createGrid(16);
 
-for (let i = 0; i < hover.length; i++)
-    hover[i].addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'gray';
-})
 
 
